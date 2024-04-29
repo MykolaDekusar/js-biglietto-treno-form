44b77e33d65890b9fafa5380f667cd07bb4967f6
MykolaDekusar <ndekusar97@gmail.com>
2024-04-29 15:54:14 +0200

    [UPD]Added Genera transformation of input in numbers

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const genera = document.getElementById('upload');
+const svuota = document.getElementById('svuota');
+let distanza = document.getElementById('distance');
+const eta = document.getElementById('age');
+const nomeUtente =
+
+    //AL CLICK DI GENERA MODIFICO I DATI IN NUMERI
+    genera.addEventListener('click', function () {
+        const distNum = Number(distanza.value);
+        const etaNum = Number(eta.value);
+        console.log(distNum);
+        console.log(etaNum);
+    })
+
+
+svuota.addEventListener('click', function () {
+    distanza.reset();
+})
