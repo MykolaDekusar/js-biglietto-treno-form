c45dd1b750806ee0e0cb82f193ed49840ce7cab3
MykolaDekusar <ndekusar97@gmail.com>
2024-04-29 18:12:26 +0200

    [FIX]Small fixes to code and readability

--- scripts/script.js
+++ scripts/script.js
@@ -1,25 +1,31 @@
 'use strict';
-const genera = document.getElementById('upload');
 const distanza = document.getElementById('distance');
-const eta = document.getElementById('age');
 const nomeUtente = document.getElementById('userName');
 const ticketToggle = document.getElementById('ticket');
-
+const age = document.getElementById('age');
 //STATIC NUMBERS
 const ticketPrice = 0.21;
 const discountU18 = 20;
 const discountO65 = 40;
-//RANDOM NUMBERS
-const nCarrozza = Math.ceil(Math.random() * 10);
-const cpCode = Math.ceil(Math.random() * 100000);
 
-//AL CLICK DI GENERA MODIFICO I DATI IN NUMERI
-genera.addEventListener('click', function () {
+//ASSIGNING VARIABLES TO TABLE
+const clientName = document.getElementById('clientName');
+const ticketType = document.getElementById('ticketType');
+const elementCarrozza = document.getElementById('nCarrozza');
+const elementCode = document.getElementById('cpCode');
+const price = document.getElementById('price');
+
+//GENERATE FUNCTION 
+function genera() {
+    //RANDOM NUMBERS
+    const nCarrozza = Math.ceil(Math.random() * 10);
+    const cpCode = Math.ceil(Math.random() * 100000);
+    //
     let distNum = Number(distanza.value);
-    const etaNum = Number(eta.value);
+    const etaNum = Number(age.value);
+    console.log(nomeUtente.value);
     console.log(distNum);
     console.log(etaNum);
-    console.log(nomeUtente.value);
     //VERIFICA INSERIMENTO DATI
     if (!(distNum <= 0) && !(etaNum < 0) && !(distNum === NaN) && !(etaNum === NaN)) {
         ticketToggle.classList.remove("display-none");
@@ -27,39 +33,38 @@ genera.addEventListener('click', function () {
         if (etaNum >= 18 && etaNum <= 65) {
             distNum = Math.round((distNum * ticketPrice) * 100) / 100;
             console.log("Il prezzo del biglietto è di " + distNum + "€");
-            document.getElementById('clientName').innerHTML = nomeUtente.value;
-            document.getElementById('ticketType').innerHTML = "Biglietto Standard";
-            document.getElementById('nCarrozza').innerHTML = nCarrozza;
-            document.getElementById('cpCode').innerHTML = cpCode;
-            document.getElementById('price').innerHTML = distNum + "€";
+            clientName.innerHTML = nomeUtente.value;
+            ticketType.innerHTML = "Biglietto Standard";
+            elementCarrozza.innerHTML = nCarrozza;
+            elementCode.innerHTML = cpCode;
+            price.innerHTML = distNum + "€";
         }
         if (etaNum < 18) {
             distNum = Math.round(((distNum * ticketPrice) - ((distNum * ticketPrice) * (discountU18 / 100))) * 100) / 100;//CALCOLO DELLO SCONTO APPROSSIMATO A 2 CIFRE DECIMALI
             console.log("Il prezzo del biglietto è di " + distNum + "€");
-            document.getElementById('clientName').innerHTML = nomeUtente.value;
-            document.getElementById('ticketType').innerHTML = "Biglietto Under 18";
-            document.getElementById('nCarrozza').innerHTML = nCarrozza;
-            document.getElementById('cpCode').innerHTML = cpCode;
-            document.getElementById('price').innerHTML = distNum + "€";
+            clientName.innerHTML = nomeUtente.value;
+            ticketType.innerHTML = "Biglietto Under 18";
+            elementCarrozza.innerHTML = nCarrozza;
+            elementCode.innerHTML = cpCode;
+            price.innerHTML = distNum + "€";
         }
         if (etaNum > 65) {
             distNum = Math.round(((distNum * ticketPrice) - ((distNum * ticketPrice) * (discountO65 / 100))) * 100) / 100;//CALCOLO DELLO SCONTO APPROSSIMATO A 2 CIFRE DECIMALI
             console.log("Il prezzo del biglietto è di " + distNum + "€");
-            document.getElementById('clientName').innerHTML = nomeUtente.value;
-            document.getElementById('ticketType').innerHTML = "Biglietto Over 65";
-            document.getElementById('nCarrozza').innerHTML = nCarrozza;
-            document.getElementById('cpCode').innerHTML = cpCode;
-            document.getElementById('price').innerHTML = distNum + "€";
+            clientName.innerHTML = nomeUtente.value;
+            ticketType.innerHTML = "Biglietto Over 65";
+            elementCarrozza.innerHTML = nCarrozza;
+            elementCode.innerHTML = cpCode;
+            price.innerHTML = distNum + "€";
         }
     } else {
         alert("Inserisci bene i dati");
     }
-})
-
-
+}
+//RESET FUNCTION FOR THE RESET BUTTON
 function svuota() {
-
+    ticketToggle.classList.add("display-none");
     nomeUtente.value = '';
     distanza.value = '';
-    eta.value = '';
+    age.value = '';
 }
